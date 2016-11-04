d260a745c9d9f5159121dc70769f1c80bb7550d8
Vasil <v.mitov.clouway@gmail.com>
2016-11-04 09:33:30 +0200

    Task6 added

---
+++ task6/calculator.js
@@ -0,0 +1,49 @@
+function concatNumber(number){
+	var value = document.getElementById("output").value;
+	if(value == 0){
+	document.getElementById("output").value = number;
+	}
+	else{
+	value=value+number;
+	document.getElementById("output").value = value;
+	}
+}
+
+function concatOperator(operator){
+	var value = document.getElementById("output").value;
+	var op=/^[0-9]+$/;
+	if(value==0){
+	document.getElementById("output").value=value;
+	}
+	else if(!value.match(op)){
+	document.getElementById("output").value=value;
+	}
+	else{
+	value=value.concat(operator);
+	document.getElementById("output").value = value;
+	}
+	theOperator=operator;
+}
+
+function equals(){
+	var operands=document.getElementById("output").value;
+	var result=eval(operands);
+	document.getElementById("output").value=result;
+	theOperator="";
+}
+
+
+function clearEverything(){
+	document.getElementById("output").value=0;
+}
+
+function clearLast(){
+	var current=document.getElementById("output").value;
+	if(current.length==1){
+	document.getElementById("output").value=0;
+	}
+	else{
+	current=current.substring(0, current.length-1);
+	document.getElementById("output").value=current;
+	}
+}
