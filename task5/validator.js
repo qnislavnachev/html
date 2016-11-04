e608729c1e1dab2c15148e28b35a1a75f9a27c09
Vasil <v.mitov.clouway@gmail.com>
2016-11-04 10:11:24 +0200

    Changes regarding task5. Code duplication removed and now the form validates on blur.

--- task5/validator.js
+++ task5/validator.js
@@ -1,75 +1,44 @@
-function f_nameCheck(){
-	var name;
-	name=document.forms['registration'].elements['firstname'].value;
+function nameCheck(formName){
+	var name=document.forms['registration'].elements[formName].value;
 		if((name.length <1)||(name.length>15)){
-		document.getElementById("f_nameVal").innerHTML = "Invalid name.";
+	    var current=document.getElementById("val").value;
+		document.getElementById("val").value = current.concat("\nNames should be between 1 and 15 charecters");
 		}
-			else
-				document.getElementById("f_nameVal").innerHTML = "";
-		
-}
-function l_nameCheck(){
-	var name;
-	name=document.forms['registration'].elements['lastname'].value;
-		if(name.length <1||name.length>15){
-		document.getElementById("l_nameVal").innerHTML = "Invalid name.";
-		}
-	else
-		document.getElementById("l_nameVal").innerHTML = "";
 }
 
 function egnCheck(){
 	var egn;
 	egn=document.forms['registration'].elements['egn'].value;
 	if(egn.length<10||egn.length>10){
-	document.getElementById("egnVal").innerHTML = "Invalid EGN.";
+	var current=document.getElementById("val").value;
+	document.getElementById("val").value = current.concat("\nEGN should be 10 digits long.");
 	}
-	else
-		document.getElementById("egnVal").innerHTML = "";
 }
 function ageCheck(){
 	var age;
 	age=document.forms['registration'].elements['age'].value;
 	if(age<18||age>118){
-	document.getElementById("ageVal").innerHTML = "Invalid age.";
+	var current=document.getElementById("val").value;
+	document.getElementById("val").value = current.concat("\nAge should be between 18 and 118.");
 	}
-	else
-		document.getElementById("ageVal").innerHTML = "";
 }
 function addressCheck(){
 	var address;
 	address=document.forms['registration'].elements['address'].value;
 	if(address.length<1||address.length>100){
-	document.getElementById("addressVal").innerHTML = "Invalid address.";
-	}
-	else
-		document.getElementById("addressVal").innerHTML = "";
-		
+	var current=document.getElementById("val").value;
+	document.getElementById("val").value = current.concat("\nAddress should be between 1 and 100 charecters.");
+	}	
 }
 function passCheck(){
 	var pass;
 	var re=/^[a-zA-Z0-9]+$/;
 	pass=document.forms['registration'].elements['pass'].value;
 	if(pass.length<6||pass.length>18||!pass.match(re)){
-	document.getElementById("passVal").innerHTML = "Invalid password.";
+	var current=document.getElementById("val").value;
+	document.getElementById("val").value = current.concat("\nPasword should be between 6 and 18 using numbers and latin charecters only.");
 	}
-	else
-		document.getElementById("passVal").innerHTML = "";
 }
-
-function c_passCheck(){
-	var c_pass;
-	var pass;
-	pass=document.forms['registration'].elements['pass'].value;
-	c_pass=document.forms['registration'].elements['c_pass'].value;
-	if(pass==c_pass){
-		document.getElementById("c_passVal").innerHTML = "";
-	}
-	else
-		document.getElementById("c_passVal").innerHTML = "Passwords dont match.";
-		
-}
-
 function validatePassConent(pass){
 	var re=new RegExp("^[a-zA-Z0-9]+$", "g");
 	if(re.test(pass)){
@@ -78,13 +47,3 @@ function validatePassConent(pass){
 	else 
 		true;
 } 
-
-function validation(){
-	f_nameCheck();
-	l_nameCheck();
-	egnCheck();
-	ageCheck();
-	addressCheck();
-	passCheck();
-	c_passCheck();
-}
