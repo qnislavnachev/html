fc4913ac411998dbbafae01d627d76dda7808268
Vasil <v.mitov.clouway@gmail.com>
2016-11-08 13:19:47 +0200

    Minor changes to task7.

--- task7/validator.js
+++ task7/validator.js
@@ -1,6 +1,6 @@
 function validateFraction(){
 	var fraction = document.getElementById("fractions").value;
-	if(fraction.match(/\b[0-9]{1,10}[.,]{1}[0-9]{1,5}\b/g)||!fraction.match(/^[a-zA-Z]/)) {
+	if(fraction.match(/\b[0-9]{1,10}[.,]{1}[0-9]{1,5}\b/g)&&	!fraction.match(/^[a-zA-Z]/)) {
 		document.getElementById("val").value = "Valid fraction.";
 	}else{
 		document.getElementById("val").value = "Invalid fraction.";
