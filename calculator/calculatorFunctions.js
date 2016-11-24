1017be28e67414cc1c52fcfea6647eb4fc0b191e
qnislavnachgev <qnislav.nachev@gmail.com>
2016-11-24 11:17:42 +0200

    injected display to the methods

--- calculator/calculatorFunctions.js
+++ calculator/calculatorFunctions.js
@@ -2,39 +2,39 @@ var dotRegex = /[.]/;
 var operationRegex = /[*/+-]/;
 var calculation = "";
 
-function enterValue(value) {
-    display.value += value;
+function enterValue(value, screen) {
+    screen.value += value;
     calculation += value; 
 }
 
-function clearLast() {
-	display.value = display.value.slice(0, display.value.length-1);
+function clearLast(screen) {
+	screen.value = screen.value.slice(0, screen.value.length-1);
 }
 
-function clearDisplay() {
-	display.value = "";
+function clearDisplay(screen) {
+	screen.value = "";
 	calculation = "";
 }
 
-function getSum() {
-	display.value = eval(calculation);
+function getSum(screen) {
+	screen.value = eval(calculation);
 }
 
-function setDot() {
+function setDot(screen) {
 	if (!dotRegex.test(display.value)) {
-		display.value += ".";
+		screen.value += ".";
 		calculation += ".";
 	}
 }
 
-function addOperand(operand) {
+function addOperand(operand, screen) {
 	var lastOperation = calculation.slice(calculation.length-1);
 	if (!operationRegex.test(lastOperation)) {
 		calculation += operand;
-		display.value = "";
+		screen.value = "";
 		return;
 	}
 	var subExpression = calculation.slice(0, calculation.length-1);
 	calculation = subExpression + operand;
-	display.value = "";
+	screen.value = "";
 }
